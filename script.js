c8809e4f42363d4e374abe4543aeb40d2dc805b9
Jessica Ringelstein <jessica.ringelstein@exxeta.com>
2024-11-08 15:34:19 +0100

    add more images and randomize

--- script.js
+++ script.js
@@ -14,4 +14,16 @@
     document.getElementById('counterdays').innerText = days;
     document.getElementById('unboxed').innerText = "0";
 
+    //declare an array to store images
+    var randomImage = new Array();
+    //0,1,2,3,4,5
+    randomImage[0] = "./img/boxes.svg";
+    randomImage[1] = "./img/boxes1.svg";
+    randomImage[2] = "./img/boxes2.svg";
+
+    // genrate a number and provide to genrate image accordingly
+    var number = Math.floor(Math.random() * randomImage.length);
+    // return the images which are genrated
+    return document.getElementById('boxes').innerHTML = '<img src="' + randomImage[number] + '" alt="Packed moving boxes" />';
+
 })();
