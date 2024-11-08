003b18e65fca947787335240fb255d29a807bb17
Jessica Ringelstein <jessica.ringelstein@exxeta.com>
2024-11-08 13:50:05 +0100

    add boxes, add counter

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
-if (document.readyState == 'loading') {
-    const canvas = document.getElementById('Matrix');
-    const context = canvas.getContext('2d');
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        week = day * 7;
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const
+        movingDay = new Date('2024-11-04T23:59:59'),
+        now = new Date(),
+        counter = now.getTime() - movingDay.getTime(),
+        days = Math.ceil(counter / day);
 
-    const latin = 'abcdefghijklmnopqrstuvwxyz';
-    const nums = '0123456789';
+    document.getElementById('counterdays').innerText = days;
+    document.getElementById('unboxed').innerText = "0";
 
-    const alphabet = latin + nums;
-
-    const fontSize = 16;
-    const columns = canvas.width / fontSize;
-
-    const rainDrops = [];
-
-    for (let x = 0; x < columns; x++) {
-        rainDrops[x] = 1;
-    }
-
-    const draw = () => {
-        context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        // context.fillStyle = '#9c8add';
-        context.fillStyle = '#7c6eb0';
-        context.font = fontSize + 'px Tengwar';
-
-        for (let i = 0; i < rainDrops.length; i++) {
-            const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-            context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
-
-            if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-                rainDrops[i] = 0;
-            }
-            rainDrops[i]++;
-        }
-    };
-
-    setInterval(draw, 30);
-}
+})();
